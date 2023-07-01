c4b57597533e29549a9bbdf9f8185f8e3a52a8cc
robert <kaukana0@tuta.io>
2023-07-01 23:00:46 +0200

    eurostat logo clickable

--- markUpCode.mjs
+++ markUpCode.mjs
@@ -15,7 +15,7 @@ export default class MarkUpCode {
     let retVal = `
 		<div style="height: 90px; display:flex; justify-content: space-between; background: url('./components/eclLikeHeroBanner/img/banner-graphical-element.svg') 65%/contain no-repeat, linear-gradient(125deg, #082b7a 35%, #AFDFE9 75%, white 100%);">
 			<p id="title" style="font-size: 2.6rem; font-weight: 700; line-height: 41px; color:white; align-self: center; margin-left:40px;">${title}</p>
-			<img src="./components/eclLikeHeroBanner/img/estat-logo-horizontal.svg" style="width:176px; margin-right:10px;"/>
+			<img src="./components/eclLikeHeroBanner/img/estat-logo-horizontal.svg" style="width:176px; margin-right:10px; cursor:pointer;" onclick="window.open('https://ec.europa.eu/eurostat','_self')"/>
 		</div>
     `
     return retVal
