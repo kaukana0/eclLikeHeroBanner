7c9147fe6a92a1bbd8a94174430d9b59002ff6e1
robert <kaukana0@tuta.io>
2023-12-04 15:57:27 +0100

    adapt title font size to screen width (WIP)

--- markUpCode.mjs
+++ markUpCode.mjs
@@ -14,13 +14,25 @@ export default class MarkUpCode {
   static getHtml(title) {
     let retVal = `
 		<div style="height: 90px; display:flex; justify-content: space-between; background: url('./components/eclLikeHeroBanner/img/banner-graphical-element.svg') 65%/contain no-repeat, linear-gradient(125deg, #082b7a 35%, #AFDFE9 75%, white 100%);">
-			<p id="title" style="font-size: 38.4px; font-weight: 700; line-height: 38.4px; color:white; align-self: center; margin-left:40px;">${title}</p>
+			<p id="title" style="font-weight: 700; color:white; align-self: center; margin-left:40px;">${title}</p>
 			<img src="./components/eclLikeHeroBanner/img/estat-logo-horizontal.svg" style="width:163.11px; height:22px; margin-right:10px; margin-top:37px; cursor:pointer;" onclick="window.open('https://ec.europa.eu/eurostat','_self')" tabindex="0"/>
 		</div>
 		<style>
-			@media (max-width:1200px)
-			.p {
-
+			p {
+				font-size: 38.4px;
+				line-height: 38.4px;
+			}
+			@media (max-width:777px) {
+				p {
+					font-size: 1.5em;
+					line-height: 1.7em;
+				}
+			}
+			@media (max-width:540px) {
+				p {
+					font-size: 1em;
+					line-height: 1.2em;
+				}
 			}
 		</style>
     `
