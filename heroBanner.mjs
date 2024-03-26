308c9fcc7aeee5a42a8c29ab5ab483a38cfa7a2c
robert <kaukana0@tuta.io>
2024-03-26 16:38:51 +0100

    new defaults for showlogo, gradient, font size cfg for backward compat

--- heroBanner.mjs
+++ heroBanner.mjs
@@ -6,7 +6,9 @@ class Element extends HTMLElement {
 	constructor() {	
 		super()	
 		this.attachShadow({mode: 'open'})
-		const tmp = MarkUpCode.getHtmlTemplate(MarkUpCode.getHtml()).cloneNode(true)
+		const height = this.style.height !== "" ? this.style.height : "128px"
+		const gradientStyle = this.hasAttribute("gradientstyle") ? this.getAttribute("gradientstyle") : "default"
+		const tmp = MarkUpCode.getHtmlTemplate(MarkUpCode.getHtml(height, gradientStyle)).cloneNode(true)
 		this.shadowRoot.appendChild(tmp)
 		this.shadowRoot.getElementById("image").addEventListener("click", e=>{
 			window.open('https://ec.europa.eu/eurostat','_self')
@@ -17,8 +19,16 @@ class Element extends HTMLElement {
 			}
 		})
 	}
+	
+	connectedCallback() {
+		if(this.hasAttribute("fontTitleSizeBase")) {
+			this.shadowRoot.getElementById("texts").style.setProperty('--font-title-size-base', this.getAttribute("fontTitleSizeBase"));
+		}
+	}
 
-	static get observedAttributes() { return ["text", "subtitle"] }
+
+
+	static get observedAttributes() { return ["text", "subtitle", "showlogo"] }
 
 	attributeChangedCallback(name, oldVal, newVal) {
 		if(name==="text") {
@@ -26,6 +36,9 @@ class Element extends HTMLElement {
 		}
 		if(name==="subtitle") {
 			this.shadowRoot.getElementById("subtitle").innerHTML = newVal
+		}
+		if(name==="showlogo") {
+			this.shadowRoot.getElementById("image").style.display = newVal==="true" ? "block" : "none"
 		}
 	}
 
--- markUpCode.mjs
+++ markUpCode.mjs
@@ -11,42 +11,84 @@ export default class MarkUpCode {
 		return t.content
 	}
 
-  static getHtml(title, subtitle) {
+  static getHtml(height, gradientStyle) {
+
+		const gradientStyleDef = {
+			"default":"linear-gradient(125deg, #082b7a 35%, #00E9FF 75%)",
+			"alt1":"linear-gradient(125deg, #082b7a 35%, #AFDFE9 75%, white 100%)"
+		}
+
     let retVal = `
-		<div id="stripe" style="height: 90px; display:flex; justify-content: space-between; background: url('./components/eclLikeHeroBanner/img/banner-graphical-element.svg') 80%/contain no-repeat, linear-gradient(125deg, #082b7a 35%, #AFDFE9 75%, white 100%);">
-			<div style="color:white; align-self: center;">
-				<p id="title" style="font-weight: 700; margin:0px 0px 0px 35px;">${title}</p>
-				<p id="subtitle" style="font-weight: 400; font-size:16px; margin:0px 0px 0px 35px;"></p>
+		<div id="stripe" style="height: ${height}; display:flex; justify-content: space-between; align-items: center; background: url('./components/eclLikeHeroBanner/img/banner-graphical-element.svg') 80%/contain no-repeat, ${gradientStyleDef[gradientStyle]};">
+			<div id="texts" style="color:white; align-self: center;">
+				<p id="title" style="margin:0px 0px 0px 35px;"></p>
+				<p id="subtitle" style="margin:0px 0px 0px 35px;"></p>
 			</div>
-			<img id="image" src="./components/eclLikeHeroBanner/img/estat-logo-horizontal.svg" style="width:163.11px; height:22px; margin-right:10px; margin-top:37px; cursor:pointer;" tabindex="0" alt="Home - Eurostat" aria-label="Home - Eurostat" />
+			<img id="image" src="./components/eclLikeHeroBanner/img/estat-logo-horizontal.svg" style="display:none; width:163.11px; height:22px; margin-right:10px; cursor:pointer;" tabindex="0" alt="Home - Eurostat" aria-label="Home - Eurostat" />
 		</div>
+
 		<style>
-			p {
-				font-size: 38.4px;
-				line-height: 38.4px;
+			#texts {
+				--font-title-size-base: 		47px;
+				--font-subtitle-size-base: 	24px;
+
+				--font-title-size-reduction1: 	 calc(var(--font-title-size-base) - 10px);
+				--font-subtitle-size-reduction1: calc(var(--font-subtitle-size-base) - 10px);
+
+				--font-title-size-reduction2: 	 calc(var(--font-title-size-base) - 17px);
+				--font-subtitle-size-reduction2: calc(var(--font-subtitle-size-base) - 12px);
+			}
+
+			#title {  
+				color: #F3F6FC;  
+				font-size: var(--font-title-size-base);
+				font-weight: bold;
+				line-height: 1.2;
+			}
+			#subtitle {
+				color: #F3F6FC;
+				font-size: var(--font-subtitle-size-base);
+				font-weight: 400;
+				line-height: 1.2;
 			}
 			@media (max-width:1200px) {
 				#stripe {
 					/*no stripe*/
-					background: linear-gradient(125deg, #082b7a 35%, #AFDFE9 75%, white 100%) !important;
+					background: ${gradientStyleDef[gradientStyle]} !important;
 				}
 			}
 			@media (max-width:777px) {
-				p {
-					font-size: 1.5em;
-					line-height: 1.7em;
+				#title {
+					font-size: var(--font-title-size-reduction1);
+					/*line-height: 1.7em;*/
+				}
+				#subtitle {
+					font-size: var(--font-subtitle-size-reduction1);
+					/*line-height: 1.7em;*/
 				}
 			}
 			@media (max-width:540px) {
-				p {
+				#title {
 					/*font-size: 1em;
 					line-height: 1.2em;*/
 				}
 				img {
-					display: none;
+					display: none !important;
 				}
 			}
-		</style>
+
+			@media (max-width:360px) {
+				#title {
+					font-size: var(--font-title-size-reduction2);
+					/*line-height: 1.7em;*/
+				}
+				#subtitle {
+					font-size: var(--font-subtitle-size-reduction2);
+					/*line-height: 1.7em;*/
+				}
+			}
+
+			</style>
     `
     return retVal
   }
